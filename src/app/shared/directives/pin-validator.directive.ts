829d23352fc48c4d01d0ba72e33cbfb2cd24a653
Vita <vita.2603.d@gmail.com>
2018-07-28 21:53:58 +0300

    Did some refactor to PIN Validator

--- src/app/shared/directives/pin-validator.directive.ts
+++ src/app/shared/directives/pin-validator.directive.ts
@@ -13,51 +13,55 @@ export class PinValidatorDirective {
 
     /*  JN-3117-5100-LH, JN-3117-5101-LJ, JN-0000-1111-AG, JN-3117-5102-LL */
 
+    function digitsSum(number) {
+      let res = 0;
+      do {
+        res += number % 10;
+        number = Math.floor(number / 10);
+      } while (number !== 0);
+      return res;
+    }
+
     function getLetterFromArray(array: any) {
-      let firstResult = 0;
-      let secondResult = 0;
+      let evenDigitsSum = 0;
+      let oddDigitsSum = 0;
       for (let i = 0; i < array.length; i++) {
         if (i % 2 === 0) {
-          firstResult += array[i] * 1;
+          /* for even indexes, the result of multiplication can never be >9 */
+          evenDigitsSum += array[i] * 1;
         } else {
-          let res = 0;
-          let num = array[i] * 2;
+          const num = array[i] * 2;
           if (num > 9) {
-            do {
-              res += num % 10;
-              num = Math.floor(num / 10);
-            } while (num !== 0);
-            secondResult += res;
+            oddDigitsSum += digitsSum(num);
           } else {
-            secondResult += num;
+            oddDigitsSum += num;
           }
         }
       }
-      return String.fromCharCode(((firstResult + secondResult) % 26) + 65);
+      return String.fromCharCode(((evenDigitsSum + oddDigitsSum) % 26) + 65);
     }
 
     const fullArray = control.value.toString().split('-');
 
-    if (fullArray.length === 4 && fullArray[0] === 'JN') {
-      const firstPartNumbers = fullArray[1].split('');
-      const secondPartNumbers = fullArray[2].split('');
-
-      if (firstPartNumbers.length === 4 && secondPartNumbers.length === 4) {
-
-        const firstLetter = getLetterFromArray(firstPartNumbers);
-        const secondLetter = getLetterFromArray(secondPartNumbers);
-        const resultLetters = firstLetter + secondLetter;
-
-        if (resultLetters === fullArray[3]) {
-          return null;
-        } else {
-          return {pin: true};
-        }
-      } else {
-        return {pin: true};
-      }
-    } else {
+    if (fullArray.length !== 4 || fullArray[0] !== 'JN') {
       return {pin: true};
     }
+
+    const firstPartNumbers = fullArray[1].split('');
+    const secondPartNumbers = fullArray[2].split('');
+
+    if (firstPartNumbers.length !== 4 && secondPartNumbers.length !== 4) {
+      return {pin: true};
+    }
+
+    const firstLetter = getLetterFromArray(firstPartNumbers);
+    const secondLetter = getLetterFromArray(secondPartNumbers);
+    const resultLetters = firstLetter + secondLetter;
+
+    if (resultLetters !== fullArray[3]) {
+      return {pin: true};
+    }
+
+    return null;
   }
 }
